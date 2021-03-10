5744918db2a6201e954ded836a4b8089bb0f0d15
rajendrrr <55138968+rajendrrr@users.noreply.github.com>
2021-03-10 20:39:19 +0530

    Add files via upload

---
+++ script/clearlist.js
@@ -0,0 +1,6 @@
+export const clearList = () => {
+    (document.getElementById('list').innerHTML = '');
+    let container = document.querySelector('ul');
+    container.parentNode.style.display = "";
+    location.reload()
+}
