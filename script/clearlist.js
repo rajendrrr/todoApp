b0eaa6133d2b842bec3cf6652497a6e016269754
rajendrrr <55138968+rajendrrr@users.noreply.github.com>
2021-03-10 20:38:39 +0530

    Delete clearlist.js

--- script/clearlist.js
+++
@@ -1,5 +0,0 @@
-export const clearList = () => {
-    (document.getElementById('list').innerHTML = '');
-    let container = document.querySelector('ul');
-    container.parentNode.style.display = "none";
-}
